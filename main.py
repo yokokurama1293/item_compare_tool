b5eba648ff78c3f62f043d4fc4fc162b5a1d91e1
yokokurama1293 <lancewest93@gmail.com>
2023-06-23 13:53:19 -0400

    added attr dropdown menus

--- main.py
+++ main.py
@@ -14,56 +14,96 @@ def main():
     # Set the title of the window
     root.title("Item Compare Tool")
 
+    # Create a notebook (tab control)
+    notebook = ttk.Notebook(root)
+    notebook.pack()
+
+    # Create the first tab
+    tab1 = ttk.Frame(notebook)
+    notebook.add(tab1, text="Compare Weapons")
+
     # Create a dropdown menu for the character type selection
     character_types = ["Barbarian", "Druid", "Necromancer", "Rouge", "Sorceress"]
     character_type = tk.StringVar()
     character_type.set(character_types[0])  # set the default option
-    character_type_menu = ttk.Combobox(root, textvariable=character_type, values=character_types)
+    character_type_menu = ttk.Combobox(tab1, textvariable=character_type, values=character_types)
     character_type_menu.pack()
 
     # Create an entry field for the DPS value
-    dps_value_entry = tk.Entry(root)
+    dps_value_entry = tk.Entry(tab1)
     dps_value_entry.pack()
 
     # Create an entry field for the number of sockets
-    sockets_entry = tk.Entry(root)
+    sockets_entry = tk.Entry(tab1)
     sockets_entry.pack()
 
     # Create dropdown menus for the user to select the attributes of the weapon
-    attributes = weapon_attributes.default_attributes + weapon_attributes.class_attributes.get(character_type, [])
     attribute1 = tk.StringVar()
-    attribute1.set(attributes[0])  # set the default option
-    attribute1_menu = ttk.Combobox(root, textvariable=attribute1, values=attributes)
+    attribute1_menu = ttk.Combobox(tab1, textvariable=attribute1)
     attribute1_menu.pack()
 
     attribute2 = tk.StringVar()
-    attribute2.set(attributes[0])  # set the default option
-    attribute2_menu = ttk.Combobox(root, textvariable=attribute2, values=attributes)
+    attribute2_menu = ttk.Combobox(tab1, textvariable=attribute2)
     attribute2_menu.pack()
 
     attribute3 = tk.StringVar()
-    attribute3.set(attributes[0])  # set the default option
-    attribute3_menu = ttk.Combobox(root, textvariable=attribute2, values=attributes)
+    attribute3_menu = ttk.Combobox(tab1, textvariable=attribute3)
     attribute3_menu.pack()
 
     attribute4 = tk.StringVar()
-    attribute4.set(attributes[0])  # set the default option
-    attribute4_menu = ttk.Combobox(root, textvariable=attribute2, values=attributes)
+    attribute4_menu = ttk.Combobox(tab1, textvariable=attribute4)
     attribute4_menu.pack()
 
     attribute5 = tk.StringVar()
-    attribute5.set(attributes[0])  # set the default option
-    attribute5_menu = ttk.Combobox(root, textvariable=attribute2, values=attributes)
+    attribute5_menu = ttk.Combobox(tab1, textvariable=attribute5)
     attribute5_menu.pack()
 
+
+    # Update the attribute dropdown menus when the character type selection changes
+    def update_attributes(*args):
+        attributes = weapon_attributes.default_attributes + weapon_attributes.class_attributes.get(character_type.get(), [])
+        attribute1.set(attributes[0])
+        attribute1_menu['values'] = attributes
+        attribute2.set(attributes[0])
+        attribute2_menu['values'] = attributes
+        attribute3.set(attributes[0])
+        attribute3_menu['values'] = attributes
+        attribute4.set(attributes[0])
+        attribute4_menu['values'] = attributes
+        attribute5.set(attributes[0])
+        attribute5_menu['values'] = attributes
+
+
+    character_type.trace('w', update_attributes)
+
     # Create a button to compare the new weapon with the currently equipped weapon
-    compare_button = tk.Button(root, text="Compare", command=lambda: compare_weapons(character_type.get(), dps_value_entry.get(), sockets_entry.get(), attribute1.get(), attribute2.get()))
+    compare_button = tk.Button(tab1, text="Compare", command=lambda: compare_weapons(character_type.get(), dps_value_entry.get(), sockets_entry.get(), attribute1.get(), attribute2.get(), attribute3.get(), attribute4.get(), attribute5.get()))
     compare_button.pack()
+
+    # Create the second tab
+    tab2 = ttk.Frame(notebook)
+    notebook.add(tab2, text="Set Weights")
+
+    # Create entry fields for the user to set the weights
+    weight1_entry = tk.Entry(tab2)
+    weight1_entry.pack()
+
+    weight2_entry = tk.Entry(tab2)
+    weight2_entry.pack()
+
+    weight3_entry = tk.Entry(tab2)
+    weight3_entry.pack()
+
+    weight4_entry = tk.Entry(tab2)
+    weight4_entry.pack()
+
+    weight5_entry = tk.Entry(tab2)
+    weight5_entry.pack()
 
     # Start the Tk event loop
     root.mainloop()
 
-def compare_weapons(character_type, dps_value, sockets, attribute1, attribute2):
+def compare_weapons(character_type, dps_value, sockets, attribute1, attribute2, attribute3, attribute4, attribute5):
     # Create a character
     char = Character('MyCharacter', character_type)
 
@@ -81,8 +121,6 @@ def compare_weapons(character_type, dps_value, sockets, attribute1, attribute2):
     attribute_weights = {
         "DPS value": int(dps_value),
         "Sockets": int(sockets),
-        attribute1: 3,
-        attribute2: 3,
     }
 
     # Compare the two weapons with the attribute weights
